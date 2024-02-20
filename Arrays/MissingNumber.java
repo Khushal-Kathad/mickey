2606a4f007583f36d65d5b6bf3efe68886263390
mickey <khushal.kathad@gmail.com>
2024-02-20 21:44:37 +0530

    Updated....

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class MissingNumber {
+    
+    public static int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int k = n * (n + 1) / 2;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        return (k - sum);
+    }
+
+    public static void main(String[] args) {
+        int[] a={3,0,1};
+        System.out.println(missingNumber(a));
+    }
+}
