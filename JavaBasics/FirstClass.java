cb75dbcdaf68c6e7e85ebef86b161261850dce91
mickey <khushal.kathad@gmail.com>
2024-07-25 11:12:45 +0530

    "java basic"

---
+++ JavaBasics/FirstClass.java
@@ -0,0 +1,13 @@
+class FirstClass{
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+}
